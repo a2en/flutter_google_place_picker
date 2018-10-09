c3e2ea543e97b37742d575f007293327ccfd2900
Miguel Ruivo <miguelpruivo@hotmail.com>
2018-10-09 17:42:31 +0100

    fixes a few issues with a plugin, make it usable again.

--- android/src/main/java/me/a2en/fluttergoogleplacepicker/FlutterGooglePlacePickerPlugin.java
+++ android/src/main/java/me/a2en/fluttergoogleplacepicker/FlutterGooglePlacePickerPlugin.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import 	java.util.HashMap;
 
+import io.flutter.app.FlutterPluginRegistry;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.plugin.common.PluginRegistry.ActivityResultListener;
 
@@ -24,49 +26,44 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import static android.app.Activity.RESULT_OK;
 
 
-public class FlutterGooglePlacePickerPlugin implements MethodCallHandler,ActivityResultListener  {
+public class FlutterGooglePlacePickerPlugin implements MethodCallHandler  {
 
-  private  final  Activity activity;
+  private static Registrar instance;
   protected GeoDataClient mGeoDataClient;
-  private  int PLACE_PICKER_REQUEST = 1;
+  private static int PLACE_PICKER_REQUEST = 1;
   protected PlaceDetectionClient mPlaceDetectionClient;
   private LatLng latLng;
   private LatLngBounds latLngBounds;
-  private  Result pendingResult;
+  private static Result pendingResult;
   /**
    * Plugin registration.
    */
-  public static void registerWith(Registrar registrar) {
+  public static void registerWith(final Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_google_place_picker");
-    final FlutterGooglePlacePickerPlugin instance = new FlutterGooglePlacePickerPlugin(registrar.activity());
-    registrar.addActivityResultListener(instance);
-    channel.setMethodCallHandler(instance);
+    channel.setMethodCallHandler(new FlutterGooglePlacePickerPlugin());
 
-  }
+    instance = registrar;
+    instance.addActivityResultListener(new PluginRegistry.ActivityResultListener(){
 
-  private FlutterGooglePlacePickerPlugin(Activity activity)
-  {
-   this.activity=activity;
-    mGeoDataClient = Places.getGeoDataClient(activity, null);
-    mPlaceDetectionClient = Places.getPlaceDetectionClient(activity, null);
-  }
-
-  @Override
-  public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == PLACE_PICKER_REQUEST) {
-      if (resultCode == RESULT_OK) {
-        Place place = PlacePicker.getPlace(activity, data);
-        HashMap<String, String> mPlace = new HashMap<>();
-        mPlace.put("latitude", place.getLatLng().latitude + "");
-        mPlace.put("longitude", place.getLatLng().longitude + "");
-        mPlace.put("id", place.getId());
-        mPlace.put("name", place.getName().toString());
-        mPlace.put("address", place.getAddress().toString());
-        pendingResult.success(mPlace);
-        return true;
+      @Override
+      public boolean onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST) {
+          if (resultCode == RESULT_OK) {
+            Place place = PlacePicker.getPlace(registrar.activity(), data);
+            HashMap<String, String> mPlace = new HashMap<>();
+            mPlace.put("latitude", place.getLatLng().latitude + "");
+            mPlace.put("longitude", place.getLatLng().longitude + "");
+            mPlace.put("id", place.getId());
+            mPlace.put("name", place.getName().toString());
+            mPlace.put("address", place.getAddress().toString());
+            pendingResult.success(mPlace);
+            return true;
+          }
+        }
+        return false;
       }
-    }
-    return false;
+    });
+
   }
 
 
@@ -79,7 +76,7 @@ public class FlutterGooglePlacePickerPlugin implements MethodCallHandler,Activit
      builder.setLatLngBounds(latLngBounds);
       */
       try {
-        activity.startActivityForResult(builder.build(activity), PLACE_PICKER_REQUEST);
+        instance.activity().startActivityForResult(builder.build(instance.activity()), PLACE_PICKER_REQUEST);
       } catch (GooglePlayServicesRepairableException e) {
         e.printStackTrace();
         result.error("Exception", e.toString(), null);
